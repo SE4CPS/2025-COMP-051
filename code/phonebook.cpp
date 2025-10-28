aef9ea303f2287187bf1d9648af84922bc7e321e
sob05001 <solomon.berhe@gmail.com>
2025-10-28 16:05:48 -0700

    Refactor Contact class and update input/output handling

--- code/phonebook.cpp
+++ code/phonebook.cpp
@@ -7,32 +7,43 @@ class Contact {
 public:
 	string number;
 	string email;
-	string name;
+	string firstname;
+	string lastname;
 
 	string getSummary() {
-		return name + " ::  " + email + " ::  " + number;
+		return firstname + " " + lastname + " ::  " + email + " ::  " + number;
 	}
 };
 
 int main()
 {
 
+	ifstream inFile("hello.txt");
+	string line;
+	while (getline(inFile, line)) {
+		cout << line << endl;
+	}
+	inFile.close();
+
+	/*
 	bool isDone = false;
-	ofstream outFile("phonebook.txt");
+	ofstream outFile("phonebook.txt", ios::app);
 	while (isDone == false) {
 
-		std::cout << "Enter number, email, and name" << std::endl;
+		std::cout << "Enter number, email, first and last name" << std::endl;
 
 		Contact c;
-		std::cin >> c.number >> c.email >> c.name;
+		std::cin >> c.number >> c.email >> c.firstname >> c.lastname;
 		outFile << c.getSummary() << endl;
 
 		std::cout << "Contact saved!" << std::endl;
-		
+
 		std::cout << "Do you wish exit the phonebook? [0,1]" << std::endl;
 		std::cin >> isDone;
 	}
 
 	outFile.close();
+	*/
+
 	return 0;
 }
